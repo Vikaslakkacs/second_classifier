7e7006114c8dde70a0df189a6e1e0d223a5d86fe
Vikas Lakka <vikaslakkacs@gmail.com>
2023-05-05 16:26:12 +0530

    initial setup for Mac

---
+++ init_setup_MacM1.sh
@@ -0,0 +1,14 @@
+echo [$(date)]: "Start"
+conda init bash
+# Installing jupyter
+echo [$(date)]: "Mac M1 steps"
+conda install -y jupyter --y
+echo [$(date)]: "De-activating conda (If using Mac M1 and trying to install tensorflow this is must)"
+echo [$(date)]:"Create env with python 3.8 version with "
+conda env create -f requirements_dev.yml
+echo [$(date)]: "activating the environment"
+conda activate secondClasifier
+echo [$(date)]: "Register for Jupyter notebook, This is for M1 Mac"
+python -m ipykernal install --user --name secondClassifier --display-name "secondClassifier tensorflow edition"
+##Installing Tensorflow in Mac M1
+echo ["$(date)"]: "End"
