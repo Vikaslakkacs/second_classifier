da51f3d989e92aa5cbc2a00add2791301f251747
Vikas Lakka <vikaslakkacs@gmail.com>
2023-05-15 23:25:03 +0530

    addition of data class for data model configuration

--- src/secondClassifier/entity/config_entity.py
+++ src/secondClassifier/entity/config_entity.py
@@ -13,3 +13,16 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
     clean_dir: Path
+
+
+##Preparing model data
+@dataclass(frozen=True)
+class PrepareBasemodelConfig:
+    root_dir:Path
+    base_model_path: Path
+    updated_model_path: Path
+    include_top: bool
+    image_size: list
+    learning_rate: float
+    weights: str
+    classes: int
